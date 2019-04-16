9621891ba086ad965008501214d0891b72bb3986
zhangzq <22748280@qq.com>
2019-04-16 14:59:57 +0800

    实现图形放大

--- src/main/java/com/ocr2x/controller/ocr2xController.java
+++ src/main/java/com/ocr2x/controller/ocr2xController.java
@@ -1,12 +1,13 @@
 package com.ocr2x.controller;
 
+import com.epam.indigo.Indigo;
+import com.epam.indigo.IndigoObject;
 import com.ggasoftware.imago.Imago;
 import org.springframework.web.bind.annotation.*;
 import sun.misc.BASE64Decoder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Date;
 import java.util.Map;
 
@@ -14,8 +15,10 @@ import java.util.Map;
 @RestController
 public class ocr2xController {
 
+    Indigo indigo = new Indigo();
     Imago imago = new Imago();
 
+    //ocr识别
     @PostMapping(value = "/ocr2x")
     public R OCR2X(@RequestParam Map<String, Object> reqMap) {
         String outMessage = "";
@@ -24,10 +27,14 @@ public class ocr2xController {
 
         BASE64Decoder decoder = new BASE64Decoder();
         byte[] picbyte = {};
-        String fileName = new Date().getTime() + ".png";
-        String upPath = "C:/Users/Avalon/Documents/upload/img/"+fileName;
+
+        Long fileName = new Date().getTime();
+        String upPath = "C:\\Users\\Avalon\\Desktop\\upload\\img\\"+fileName+".png";
+        String upPath2x = "C:\\Users\\Avalon\\Desktop\\upload\\img\\"+fileName+"_[NS-L3][x2.000000].png";
         String mol = "";
 
+        String strCmd = "C:\\waifu2x\\waifu2x --model-dir \"C:\\waifu2x\\models_rgb\" --scale-ratio \"2.0\" --noise-level \"3\" -i \""+upPath+"\"";
+        System.out.println(strCmd);
         try {
             picbyte = decoder.decodeBuffer(picdata);
             FileOutputStream fos = new FileOutputStream(upPath);
@@ -35,8 +42,19 @@ public class ocr2xController {
             fos.flush();
             fos.close();
 
+            Process p = Runtime.getRuntime().exec(strCmd);
+            InputStream is = p.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"gb2312"));
+            String line;
+            while((line = reader.readLine())!= null){
+                System.out.println(line);
+            }
+            is.close();
+            reader.close();
+            p.destroy();
+
             //ocr识别
-            imago.loadImage(upPath);
+            imago.loadImage(upPath2x);
             imago.filterImage();
             imago.recognize();
             mol = imago.getResultMolecule();
@@ -45,7 +63,21 @@ public class ocr2xController {
         }
 
 
-        return R.ok().put("img2x",fileName).put("mol",mol);
+        return R.ok().put("img2x",fileName+".png").put("mol",mol);
+    }
+
+    //如果没有info方法 不会显示 高级功能按钮
+    @GetMapping("/info")
+    public R info() {
+        return R.ok().put("indigo_version", indigo.version()).put("imago_versions", imago.getVersion());
+    }
+
+    //整理
+    @PostMapping("/layout")
+    public R layout(@RequestBody Map<String, Object> reqMap) {
+        IndigoObject mol = indigo.loadMolecule((String) reqMap.get("struct"));
+        mol.layout();
+        return R.ok().put("struct", mol.molfile());
     }
 
 }
--- src/test/java/com/ocr2x/ApplicationTests.java
+++ src/test/java/com/ocr2x/ApplicationTests.java
@@ -5,12 +5,32 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        try {
+            Process p = Runtime.getRuntime().exec("C:\\Users\\Avalon\\Downloads\\waifu2x\\waifu2x --model-dir \"C:\\Users\\Avalon\\Downloads\\waifu2x\\models_rgb\" --scale-ratio \"2.0\" --noise-level \"3\" -i \"C:\\Users\\Avalon\\Pictures\\1.png\"");
+            InputStream is = p.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"gb2312"));
+            String line;
+            while((line = reader.readLine())!= null){
+                System.out.println(line);
+            }
+            is.close();
+            reader.close();
+            p.destroy();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
